b243d88328e3ff2a22b68b7e296355a5f6af894f
Colton Doling <cmdoling@gmail.com>
2024-11-03 19:11:54 -0500

    created function to render the license section and implemented it in the markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,7 +29,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return ''
+  } else {
+  return `## License
+  
+  This project is licensed under the ${renderLicenseLink(license)} license.
+
+  `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -58,9 +68,7 @@ function generateMarkdown(data) {
 
   ${data.usage}
 
-  ## License
-
-
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
 
